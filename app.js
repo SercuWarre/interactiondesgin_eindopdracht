e5fd6cff8ca65b33664c98c377d610d196a5fcdc
warresercu <warre.sercu@student.howest.be>
2022-12-21 10:05:37 +0100

    design

---
+++ app.js
@@ -0,0 +1,60 @@
+
+
+ async function getPlayerInfo () {
+  //   const apiKey = "7pe7uupaubprfbr96au42ffb";
+  //   const playerID = "41c44740-d0f6-44ab-8347-3b5d515e5ecf";
+  const url = `https://api.sportradar.com/handball/trial/v2/en/players/sr:player:1289838/profile.json?api_key=7pe7uupaubprfbr96au42ffb`;
+  const config = {
+    method: 'GET',
+    mode: 'no-cors',
+    // headers: { 'Access-Control-Allow-Origin': '*' },
+  };
+  const response = await fetch(url, config);
+  // const data = await response.json();
+  console.log(response);
+  return response;
+  
+}
+     
+      
+      
+
+      
+ 
+function randomizeValues() {
+  // Generate random id and assign them to variables
+  var value = Math.floor(Math.random() * 100);
+  document.querySelector('.js-nummer').innerHTML = value;
+  var value1 = Math.floor(Math.random() * 100);
+  var value2 = Math.floor(Math.random() * 100);
+  var value3 = 100
+  console.log(value1, value2, value3);
+  var progress1 = value1 / value3 * 100;
+  var progress=value2/value3*100;
+  progress = Math.round(progress, 2);
+  progress1 = Math.round(progress1, 2);
+  console.log(progress);
+  console.log(progress1);
+  // Update the values on the page
+  // document.getElementById('progress1').value = value1;
+  // document.getElementById('progress').value = value2;
+  // document.getElementById('progress') = value2;
+  document.getElementById('js-progresss').innerHTML = `${progress}%`;
+  document.getElementById('js-progresss1').innerHTML = `${progress1}%`;
+ 
+    var element = document.getElementById('js-progress');
+    var width = progress;
+    element.style.width = width + '%';
+    var element1 = document.getElementById('js-progress1');
+    var width1 = progress1;
+    element1.style.width = width1 + '%';
+}
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+  console.log('Test');
+
+  const data=  getPlayerInfo();
+  console.log(data);
+  
+});
